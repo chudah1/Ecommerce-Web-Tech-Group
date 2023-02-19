4c4418519544777cf3702f11a02430ad261504ef
Chudah <yakung25c@gmail.com>
2023-02-19 06:44:34 +0000

    refactored

---
+++ templatemo_559_zay_shop/includes/footer.php
@@ -0,0 +1,78 @@
+ <!-- Start Footer  (remove all links)-->
+ <footer class="bg-dark" id="tempaltemo_footer">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-md-4 pt-5">
+                    <h2 class="h2 text-success border-bottom pb-3 border-light logo" style="color: #cfd6e1 !important;">Tranquillo</h2>
+                    <ul class="list-unstyled text-light footer-link-list">
+                        <li>
+                            <i class="fas fa-map-marker-alt fa-fw"></i>
+                            1 University Ave
+                        </li>
+                        <li>
+                            <i class="fa fa-phone fa-fw"></i>
+                            <a class="text-decoration-none">010-020-0340</a>
+                        </li>
+                        <li>
+                            <i class="fa fa-envelope fa-fw"></i>
+                            <a class="text-decoration-none">tanquillo@company.com</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="col-md-4 pt-5">
+                    <h2 class="h2 text-light border-bottom pb-3 border-light">Products</h2>
+                    <ul class="list-unstyled text-light footer-link-list">
+                        <li><a class="text-decoration-none">Luxury</a></li>
+                        <li><a class="text-decoration-none">Sport Wear</a></li>
+                        <li><a class="text-decoration-none">Men's Shoes</a></li>
+                        <li><a class="text-decoration-none">Women's Shoes</a></li>
+                        <li><a class="text-decoration-none">Popular Dress</a></li>
+                        <li><a class="text-decoration-none">Gym Accessories</a></li>
+                        <li><a class="text-decoration-none">Sport Shoes</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-md-4 pt-5">
+                    <h2 class="h2 text-light border-bottom pb-3 border-light">Further Info</h2>
+                    <ul class="list-unstyled text-light footer-link-list">
+                        <li><a class="text-decoration-none">Home</a></li>
+                        <li><a class="text-decoration-none">About Us</a></li>
+                        <li><a class="text-decoration-none">Shop Locations</a></li>
+                        <li><a class="text-decoration-none">FAQs</a></li>
+                        <li><a class="text-decoration-none">Contact</a></li>
+                    </ul>
+                </div>
+
+            </div>
+
+            <div class="row text-light">
+                <div class="col-12 mb-3">
+                    <div class="w-100 my-3 border-top border-light"></div>
+                </div>
+
+
+                <div class="w-100 bg-black py-3">
+                    <div class="container">
+                        <div class="row pt-2">
+                            <div class="col-12">
+                                <p class="text-left text-light">
+                                    Copyright &copy; <?php echo date("Y");?> Traquillo
+                                    | Designed by <a rel="sponsored" href="https://templatemo.com" target="_blank">Tranquillo Team</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+    </footer>
+    <!-- End Footer -->
+
+    <!-- Start Script -->
+    <script src="assets/js/jquery-1.11.0.min.js"></script>
+    <script src="assets/js/jquery-migrate-1.2.1.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/templatemo.js"></script>
+    <script src="assets/js/custom.js"></script>
+    <!-- End Script -->
